a70e4275eeb778c5533c15b7d379e528e5c9817f
kevdie <kevindiegofloresantezana@hotmail.com>
2020-02-06 20:32:49 -0400

    pp

---
+++ IntroClassCShar/IntroClassCShar/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace IntroClassCShar
+{
+    public class Shape
+    {
+        public string Name { get; set; }
+        public string Color { get; set; }
+        public virtual void Show()
+        {
+            //Console.WriteLine(string.Format("my name is {0} shape", name));
+            Console.WriteLine($"my name is {Name} shape");
+        }
+    }
+    public class Square:Shape
+    {
+        public override void Show()
+        {
+            Console.WriteLine($"im a square {Name}");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Shape myShape = new Shape("basic");
+            var myShape = new Shape() { Name="obj"};
+            myShape.Show();           
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
